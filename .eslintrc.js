f2f554068855daeb247771315bcaf28d3d130fac
alex-wwww <a20185462@pucp.edu.pe>
2023-01-07 12:45:10 -0500

    Componentes Lazy agregados

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+
+  rules: { 'linebreak-style': 0, 'react/react-in-jsx-scope': 'off' },
+};
